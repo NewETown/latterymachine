75935ca3841788df8d407de32c00b63b75379ba2
Everett Carney <everett@mentaltangent.net>
2015-03-11 08:26:16 -0700

    Almost done

--- js/lattery.js
+++ js/lattery.js
@@ -1,14 +1,28 @@
+// Each offset is the same per panel, offsets are in degrees
+// Offsets are calculated by 360 / 3 = 120 degree viewport per panel
+var COFFEE_OFFSET = 0;
+var TEA_OFFSET = 120;
+var ESPRESSO_OFFSET = 240
+
 $(document).ready(function() {
-    $('.btn').click(function() {
+
+    $('.go-button').click(function() {
+        $(this).addClass('paused');
+        
         var first = spinReel();
         var second = spinReel();
         var third = spinReel();
+        
+        spin(first, '#cup');
+        spin(second, '#filter');
+        spin(third, '#ingredients');
         
         if (first === second && second === third)
             youMadeCoffee();
         else
             sad();
     });
+    
 });
 
 function spinReel() {
@@ -23,6 +37,26 @@ function sad() {
     console.log(':(');
 }
 
+function spin(num, el) {
+    // Generate the number of times to spin
+    var offset = 1 + (num/3);
+    console.log(offset);
+    
+    // Need to add animation for each browser
+    var webkit = "-webkit-";
+    var moz = "-moz-";
+    var ms = "-ms-";
+    var animation = "animation-iteration-count: " + offset + ";";
+    
+    $(el).attr('style', webkit + animation + " " + moz + animation + " " + ms + animation + " " + animation);
+    $(el).addClass('spin');
+    
+}
+
+function reset() {
+    $('.go-button').removeClass('paused');
+}
+
 /*
 function lattery() {
     entry function
@@ -31,18 +65,9 @@ function lattery() {
     sets timeout for clear function popup
 }
 
-function spin(offset, el) {
-    returns nothing
-    spins the element starting at the given offset (reel * 250ms?)
-}
-
 function spawnClearModal() {
     After timeout, spawn a modal at the top of .coffee-container that allows the user to reset the machine
     On 'reset' button click: reset();
-}
-
-function reset() {
-    resets all elements and counters
 }
 
 */
